1940f2d0c41c64d09a98f5bc72cad4308dbeae43
eschencode <linuseschenbach@gmail.com>
2026-05-08 17:09:25 +0200

    add icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,15 @@
 	import './layout.css';
 	import { page } from '$app/stores';
 	import Footer from '$lib/Footer.svelte';
+	import signIcon from '$lib/assets/images/signiture.png';
 
 	let { children } = $props();
 	let isHome = $derived($page.url.pathname === '/');
 </script>
+
+<svelte:head>
+	<link rel="icon" href={signIcon} type="image/png" />
+</svelte:head>
 
 {@render children()}
 {#if !isHome}
